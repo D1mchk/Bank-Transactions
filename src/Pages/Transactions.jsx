9e406316f6a6b2f7b4e9291b81ceb30632c4fa97
D1mchk <dmitryloznuha@gmail.com>
2026-08-19 18:50:20 +0200

    feat: update account summary cards with dynamic balance display

--- src/Pages/Transactions.jsx
+++ src/Pages/Transactions.jsx
@@ -7,9 +7,7 @@ function Transactions() {
   const dispatch = useDispatch();
 
   const transactions = useSelector((state) => state.transactions.items);
-
   const accounts = useSelector((state) => state.accounts.items);
-
   const selectedAccountId = useSelector(
     (state) => state.accounts.selectedAccountId
   );
@@ -17,17 +15,69 @@ function Transactions() {
   const selectedTransactions = transactions.filter(
     (transaction) => selectedAccountId == transaction.accountId
   );
+  const selectedAccount = accounts.find(
+    (account) => selectedAccountId == account.id
+  );
 
-  console.log(selectedTransactions);
+  const positiveTransactions = selectedTransactions.filter(
+    (transaction) => transaction.type == "debit"
+  );
+  const negativeTransactions = selectedTransactions.filter(
+    (transaction) => transaction.type == "credit"
+  );
 
+  const sumPositiveTransactions = positiveTransactions.reduce(
+    (sum, account) => sum + account.amount,
+    0
+  );
+  const sumNegativeTransactions = negativeTransactions.reduce(
+    (sum, account) => sum + account.amount,
+    0
+  );
+
+  console.log(positiveTransactions);
+  console.log(sumPositiveTransactions);
   return (
     <div className="my-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <CardInfo>qwerty</CardInfo>
-        <CardInfo>123</CardInfo>
-        <CardInfo>1234567890</CardInfo>
-      </div>
-      <div>
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-5">
+        <CardInfo>
+          <div className="text-xs uppercase tracking-wider text-neutral-500">
+            current balance
+          </div>
+
+          <div className={`text-3xl font-bold font-mono text-purple-400`}>
+            $
+            {selectedAccount
+              ? selectedAccount.balance < 0
+                ? selectedAccount.balance * -1
+                : selectedAccount.balance
+              : "0.00"}
+          </div>
+        </CardInfo>
+        <CardInfo>
+          <div className="text-xs uppercase tracking-wider text-neutral-500">
+            money in
+          </div>
+          <div className={`text-3xl font-bold font-mono text-cyan-400`}>
+            ${sumPositiveTransactions}
+          </div>
+          <div className="text-xs tracking-wider text-neutral-500">
+            {positiveTransactions.length} transactions
+          </div>
+        </CardInfo>
+        <CardInfo>
+          <div className="text-xs uppercase tracking-wider text-neutral-500">
+            money out
+          </div>
+          <div className={`text-3xl font-bold font-mono text-red-400`}>
+            ${sumNegativeTransactions}
+          </div>
+          <div className="text-xs tracking-wider text-neutral-500">
+            {negativeTransactions.length} transactions
+          </div>
+        </CardInfo>
+      </section>
+      <section className="mb-5">
         {accounts.map((account) => (
           <CardButton
             key={account.id}
@@ -36,12 +86,12 @@ function Transactions() {
             {account.last4}
           </CardButton>
         ))}
-        <div>
-          {selectedTransactions.map((transaction) => (
-            <CardInfo>{transaction.merchant}</CardInfo>
-          ))}
-        </div>
-      </div>
+      </section>
+      <section>
+        {selectedTransactions.map((transaction) => (
+          <CardInfo key={transaction.id}>{transaction.merchant}</CardInfo>
+        ))}
+      </section>
     </div>
   );
 }
